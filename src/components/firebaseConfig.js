7bb441c185b854210e901124bc27f8e75eb98945
Thiago Cruz <thagocruz@gmail.com>
2018-05-15 06:25:51 -0300

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyDEIk_StlXuyRW0H4BkFbu-uOwMrJyGJuA",
-    authDomain: "crud-vue-a155d.firebaseapp.com",
-    databaseURL: "https://crud-vue-a155d.firebaseio.com",
-    projectId: "crud-vue-a155d",
-    storageBucket: "crud-vue-a155d.appspot.com",
-    messagingSenderId: "528699623149"
-}
