a1db9f07940236cef9bfe4362866641afedb082c
Thiago Cruz <thagocruz@gmail.com>
2018-05-15 06:23:47 -0300

    Add files via upload

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+      <div class="nav-wrapper green">
+          <div class="container">
+              <router-link to="/" class="brand-logo">Employee Menager</router-link>
+          </div>
+      </div>
+  </nav>
+</template>
